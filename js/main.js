c0db9ae9dc0d85ba096ecbdd22cf4fe8180a28a7
HungBT <hung.bui.thanh@ntq-solution.com.vn>
2018-10-02 11:05:21 +0700

    fix feedback

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.list_slide').slick({
         dots: false,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 5000,
         arrows: true,
     });
