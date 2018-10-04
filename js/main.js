7ffd9c23a2c4426b3ee490cecf0767994a0e6f7f
HungBT <hung.bui.thanh@ntq-solution.com.vn>
2018-10-04 09:48:14 +0700

    update slider

--- js/main.js
+++ js/main.js
@@ -6,6 +6,19 @@ $(document).ready(function(){
         arrows: true,
     });
 
-    $(".list_slide .slick-next").html("");
-    $(".list_slide .slick-prev").html("");
+    // $('#list-content').slick({
+    //     dots: false,
+    //     autoplay: false,
+    //     autoplaySpeed: 5000,
+    //     arrows: true,
+    // });
+
+    $(".slick-next").html("");
+    $(".slick-prev").html("");
+
+    $('.carousel').carousel({
+        pause: true,
+        interval: false
+    });
+
 });
